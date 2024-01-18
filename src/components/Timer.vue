5d15954bca600abe2329ece92e269a942b35932b
Angel DLT <mdelatorre3@ucol.mx>
2024-01-17 20:19:53 -0600

    timer numbers and buttons  added

---
+++ src/components/Timer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <div class="timer text-center">{{ formattedTime }}</div>
+      <div class="buttons">
+        <button @click="startTimer">Iniciar</button>
+        <button @click="pauseTimer">Pausar</button>
+        <button @click="resetTimer">Limpiar</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        currentTime: 0,
+      };
+    },
+    computed: {
+      formattedTime() {
+        const hours = Math.floor(this.currentTime / 3600);
+        const minutes = Math.floor((this.currentTime % 3600) / 60);
+        const seconds = this.currentTime % 60;
+  
+        return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`;
+      },
+    },
+    methods: {
+      pad(value) {
+        return value.toString().padStart(2, '0');
+      },
+    },
+  };
+  </script>
+  
--- src/views/TimerPage.vue
+++ src/views/TimerPage.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
-        <Navigation />
+      <Navigation />
+      <div class="flex flex-col justify-center items-center">
+          <h1 class=" text-4xl">Timer</h1>
+          <Timer />
+      </div>
     </div>
-</template>
-
-<script>
-    import Navigation from '@/components/Navigation.vue';
-
-    export default {
+  </template>
+  
+  <script>
+  import Navigation from '@/components/Navigation.vue';
+  import Timer from '@/components/Timer.vue';
+  
+  export default {
     components: {
-        Navigation,
+      Navigation,
+      Timer,
     },
-    };
-</script>
-
-<style scoped>
-
-</style>
+  };
+  </script>
+  
