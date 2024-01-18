feb3a1f985bf9322ca15e1c28bf963fa9869aac7
Angel DLT <mdelatorre3@ucol.mx>
2024-01-17 18:06:43 -0600

    loading simbol added, pages auth need added

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,35 @@
-// import vue-router to create routes.
 import { createRouter, createWebHistory } from 'vue-router';
-
-// import the components that will be used as pages.
 import LoginPage from '../views/LoginPage.vue';
 import TodoListPage from '../views/TodoListPage.vue';
 import TimerPage from '../views/TimerPage.vue';
 import GitProfilePage from '../views/GitProfilePage.vue';
 
-// create routes.
 const routes = [
-    { path: '/', redirect: '/login' }, // Redirige la ruta raíz a /login
-    { path: '/login', component: LoginPage },
-    { path: '/todo-list', component: TodoListPage },
-    { path: '/timer', component: TimerPage },
-    { path: '/git-profile', component: GitProfilePage },
-  ];
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: LoginPage },
+  { path: '/todo-list', component: TodoListPage, meta: { requiresAuth: true } }, // Agrega meta.requiresAuth
+  { path: '/timer', component: TimerPage, meta: { requiresAuth: true } }, // Agrega meta.requiresAuth
+  { path: '/git-profile', component: GitProfilePage, meta: { requiresAuth: true } }, // Agrega meta.requiresAuth
+];
 
-// create router.
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 
-// export router.
+// Agrega un guard de navegación
+router.beforeEach((to, from, next) => {
+  // Verifica si la ruta requiere autenticación
+  if (to.meta.requiresAuth) {
+    // Verifica la existencia de la cookie
+    const isLoggedIn = document.cookie.includes('logginSuccess=true');
+    if (!isLoggedIn) {
+      // Redirige a la página de inicio de sesión si no está autenticado
+      next('/login');
+    }
+  }
+  // Continúa con la navegación si no se requiere autenticación o si está autenticado
+  next();
+});
+
 export default router;
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -11,6 +11,15 @@
       <o-input v-model="username" placeholder="Usuario" class="mb-4 w-full border p-3 rounded-sm shadow-md shadow-slate-800" />
       <o-input v-model="password" type="password" placeholder="Contraseña" class="mb-4 w-full border p-3 rounded-sm shadow-md shadow-slate-800" />
       <o-button @click="login" class="text-white px-6 py-3 rounded-full shadow-md hover:shadow-lg hover:shadow-slate-900 shadow-slate-900 transition duration-300">Iniciar Sesión</o-button>
+      
+      <div v-if="loading" class="sk-chase mt-4">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+      </div>
       <p v-if="error" class="mt-4 text-white bg-gradient-to-br from-red-800 to-red-600 py-2 px-6 text-center shadow-md shadow-slate-900 rounded-md">{{ error }}</p>
     </div>
   </div>
@@ -27,11 +36,16 @@ export default {
       username: '',
       password: '',
       error: null,
+      loading: false,
     };
   },
   methods: {
     async login() {
       try {
+
+        this.loading = true;
+        this.error = null;
+
         const hashedPassword = md5(this.password);
         const response = await axios.get('https://mocki.io/v1/3e408794-39ed-4c75-bb6e-c49c578de293');
         const {userName, password} = response.data;
@@ -44,6 +58,9 @@ export default {
         }
       } catch (error) {
         this.error = 'Error en la llamada a la API';
+      }
+      finally {
+        this.loading = false;
       }
     },
   },
