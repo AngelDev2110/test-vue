4ddbb1d543d3270ee84389f9ca67842d32b91aa8
Angel DLT <mdelatorre3@ucol.mx>
2024-01-17 18:22:02 -0600

    navigation bar added

---
+++ src/components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="bg-gray-900 py-4">
+      <div class="flex items-center justify-center">
+        <div class="flex items-center flex-col space-y-1 sm:flex-row sm:space-x-6">
+          <router-link to="/todo-list" class="text-white text-2xl hover:text-blue-400">TodoList</router-link>
+          <router-link to="/timer" class="text-white text-2xl hover:text-blue-400">Timer</router-link>
+          <router-link to="/git-profile" class="text-white text-2xl hover:text-blue-400">GitProfile</router-link>
+          <button class="text-white text-2xl hover:text-red-500">Cerrar sesión</button>
+        </div>
+      </div>
+    </nav>
+</template>
--- src/views/TodoListPage.vue
+++ src/views/TodoListPage.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-
+        <Navigation />
     </div>
 </template>
 
 <script>
+    import Navigation from '@/components/Navigation.vue';
+
     export default {
-        
-    }
+    components: {
+        Navigation,
+    },
+    };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
